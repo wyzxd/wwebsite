4c393fc8ac9628a0f606a7d5c2a4cf8a572fbc8c
Арсений <onelovecorgi2016@gmail.com>
2025-03-21 23:53:50 +0500

    ff

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return HttpResponse('<h4>fimozz<h4>')
+def svo(request):
+    return HttpResponse('<h2>ZXCFIMOZZZ<h2>')
+# Create your views here.
