5560d562c53d352200d23b3f918ea93aa01cf3e3
Арсений <onelovecorgi2016@gmail.com>
2025-03-23 16:31:37 +0500

    g

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', name="home")
 def reg(request):
-    return render(request, 'main/index.html/')
+    return render(request, 'main/index.html/', name='reg')
 # Create your views here.
