512e381dfa43a96d8788ad07a9238737b8898f98
Арсений <onelovecorgi2016@gmail.com>
2025-03-23 15:08:27 +0500

    +http

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('about', views.svo)
+    path('about', views.reg)
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h4>fimozz<h4>')
-def svo(request):
-    return HttpResponse('<h2>ZXCFIMOZZZ<h2>')
+    return render(request, 'main/index.html')
+def reg(request):
+    return render(request, 'main/index.html/')
 # Create your views here.
